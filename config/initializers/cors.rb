3451276e2be4ad0ee01e1f5247a87bd12fccf062
Helio Machado <0x2b3bfa0+git@googlemail.com>
2025-02-28 18:45:39 +0100

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,15 @@
-# config/initializers/cors.rb
+# config/initializers/cross_origin.rb
 # ref: https://github.com/cyu/rack-cors
 
 # font cors issue with CDN
-# Ref: https://stackoverflow.com/questions/56960709/rails-font-cors-policy
+# ref: https://stackoverflow.com/questions/56960709/rails-font-cors-policy
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://chatwoot.studio.datachain.ai', 'https://studio.datachain.ai', %r{^https?://(.*.?)chatwoot.com$}
+    origins '*'
     resource '/packs/*', headers: :any, methods: [:get, :options]
     resource '/audio/*', headers: :any, methods: [:get, :options]
     # Make the public endpoints accessible to the frontend
-    resource '/public/api/*', headers: :any, methods: :any, credentials: true
+    resource '/public/api/*', headers: :any, methods: :any
 
     if ActiveModel::Type::Boolean.new.cast(ENV.fetch('CW_API_ONLY_SERVER', false)) || Rails.env.development?
       resource '*', headers: :any, methods: :any, expose: %w[access-token client uid expiry]
@@ -17,11 +17,23 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 end
 
+class CrossOriginPolicies
+  def initialize(app)
+    @app = app
+  end
 
-Rails.application.config.middleware.use Rack::Config do |env|
-  env['cross-origin-embedder-policy'] = 'credentialless'
-  env['cross-origin-resource-policy'] = 'cross-origin'
+  def call(env)
+    status, headers, response = @app.call(env)
+
+    headers['cross-origin-embedder-policy'] = 'credentialless'
+    headers['cross-origin-resource-policy'] = 'cross-origin'
+    headers['content-security-policy'] = "frame-ancestors 'self' https://studio.datachain.ai;"
+
+    [status, headers, response]
+  end
 end
+
+Rails.application.config.middleware.insert_before 0, CrossOriginPolicies
 
 ################################################
 ######### Action Cable Related Config ##########
